eeebfe31bf139f52b8e124b84247f74807060aa2
Liz <lizwangying@icloud.com>
2023-03-27 16:37:17 +0800

    git rm -r --cached .

--- src/main/java/org/example/BasicDateStructureTraining.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-/**
- * @Desc  : 数据结构练习II
- * @Author  : Liz</p>
- * @Date  : 2023/3/22</p>
- */
-public class BasicDateStructureTraining {
-}
--- src/main/java/org/example/CodingAblityTraining.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-/**
- * @Desc : 编程能力
- * @Author : Liz</p>
- * @Date : 2023/3/22</p>
- */
-public class CodingAblityTraining {
-}
